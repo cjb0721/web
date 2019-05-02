e1ffbd5d5c8a562b413d3854960edcfe143c64a1
qtxz <1451055806@qq.com>
2019-05-02 12:17:04 +0800

    创建模型生成迁移文件并执行迁移文件

--- webmonitor/webapp/models.py
+++ webmonitor/webapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 主机表
+class Host_info(models.Model):
+    app_name = models.CharField(max_length=40)
+    url = models.CharField(max_length=100)
+    idc = models.CharField(max_length=3)
+    alarm_type = models.CharField(max_length=8)
+    alarm_info = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.app_name
+
+
+# 监控数据表
+class Monitor_data(models.Model):
+    fid = models.ForeignKey('Host_info', on_delete=models.CASCADE)
+    dns_lookup_time = models.FloatField()
+    connect_time = models.FloatField()
+    pre_transfer_time = models.FloatField()
+    start_transfer_time = models.FloatField()
+    total_time = models.FloatField()
+    http_status = models.CharField(max_length=100)
+    size_download = models.FloatField()
+    size_header = models.SmallIntegerField()
+    size_request = models.SmallIntegerField()
+    download_length = models.FloatField()
+    download_speed = models.FloatField()
+    datetime = models.DateTimeField(auto_now_add=True, max_length=12)
+    mark = models.CharField(max_length=1)
+
